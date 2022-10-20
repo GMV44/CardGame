821c3b7d5be6ca0b04c771e01862fd7fe2255ca3
root <root@LAPTOP-B6VQPMKU.localdomain>
2022-10-20 13:29:26 -0500

    html css and js created

---
+++ index.js
@@ -0,0 +1,27 @@
+class Card {
+    constructor(rank, suit) {
+        this.rank = rank;
+        this.suit = suit;
+    }
+}
+
+class Hand {
+    constructor() {
+        this.arrayOfCards = [];
+    }
+    addCard(card) {
+        this.arrayOfCards.push(card)
+    }
+    playCard(card) {
+        
+    }
+}
+
+class Deck {
+    constructor() {
+    
+        
+
+    }
+} 
+
